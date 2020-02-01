73d8130dc9bba381a77b0823ba3b0c9a38a9d5a5
eesayas <isaiasgalindobriones@gmail.com>
2020-02-01 18:37:18 +0000

    half of homepage done

--- public/javascripts/partials/navbar.js
+++ public/javascripts/partials/navbar.js
@@ -1,14 +1,36 @@
+//on logo click
 $('#logo-cont').click( function() {
     window.location.href = "/";
 });
 
-if( $(window).scrollTop() ){
-    $('#logo-cont').css('width', '20rem');
+//generalize
+function scrolledNav(){
+    $('#logo-cont>img').attr('src', '/images/RBCE-LOGO.png');
+    $('#logo-cont').css({
+        width: '15rem',
+        transition: '0.3s'
+    });
+    $('#navbar-cont').css('background', 'white');
+    $('#links-cont > .link').css('color', 'black');
+    $('#contact-us-btn').css({
+        background: '#022d41',
+        color: 'white'
+    });
+    $('#links-cont > .link::after').css({
+        background: 'black !important'
+    });
 }
 
+//on init
+if( $(window).scrollTop() ){
+    scrolledNav();
+}
+
+//on scroll
 $(window).on('scroll', function(){
 
     if( !$(window).scrollTop() ){
+        $('#logo-cont>img').attr('src', '/images/RBCE-LOGO-white.png');
         $('#logo-cont').css({
             width: '20rem',
             transition: '0.3s'
@@ -16,21 +38,17 @@ $(window).on('scroll', function(){
 
         $('#navbar-cont').css({
             background: 'none',
-            // opacity: '1'
+            opacity: '1 !important'
         });
 
-       
+        $('#links-cont > .link').css('color', 'white');
+        $('#contact-us-btn').css({
+            background: 'white',
+            color: 'black'
+        });   
     
-    } else{
-        $('#logo-cont').css({
-            width: '15rem',
-            transition: '0.3s'
-        });
-
-         $('#navbar-cont').css({
-            background:'rgba(255,255,255,0.7)',
-            // opacity: '0.3'
-        });
+    } else{ //scroll nav is not on top of page
+        scrolledNav();
     }
     
 });
