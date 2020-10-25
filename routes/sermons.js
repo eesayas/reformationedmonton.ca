6742c4ff3790acfbad282ab176b078a4a8d32e5e
eesayas <briones@ualberta.ca>
2020-10-24 19:52:55 -0600

    setup callback

--- routes/sermons.js
+++ routes/sermons.js
@@ -4,5 +4,8 @@ const { indexSermons, showSermon } = require('../controllers/sermons');
 
 router.get('/', indexSermons);
 router.get('/:date', showSermon);
+router.post("/", (req, res, next) => {
+    console.log(req.body);
+});
 
 module.exports = router;
